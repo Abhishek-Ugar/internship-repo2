418300a66cd3d3ff504ca1fdbacfd9c06b074bca
abhishek <abhishekugar64@gmail.com>
2026-01-07 00:18:55 +0530

    task-01 complited

---
+++ 06-01-task/bill.py
@@ -0,0 +1,14 @@
+def bill(units):
+    if units < 100 :
+        res = units * 2
+        print(res)
+    elif units > 100 and units < 200 :
+        res = units * 4
+        print(res)
+    elif units > 200 :
+        res = units * 6
+        print(res)
+    else:
+        print("enter valid input..!")
+
+bill(int(input()))
---
+++ 06-01-task/eligibilty.py
@@ -0,0 +1,9 @@
+# If age ≥ 18 and percentage ≥ 60 → Eligible
+# Else → Not Eligible
+def CIE(age, percentage):
+    if age >= 18 and percentage >= 60 :
+        print("your eligible..!")
+    else:
+        print("your not eligible..!")
+
+CIE(12, 50)
